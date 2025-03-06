49bd6f696dcaf170504e9bd69de22e26b7318c59
Coo <80071732+Coo29@users.noreply.github.com>
2025-03-06 12:30:18 -0500

    Beta deck enabled
    unfinished, doesn't have a finalized texture and is mostly just for testing.

--- Yippie/scripts/decks.lua
+++ Yippie/scripts/decks.lua
@@ -1,31 +1,31 @@
 SMODS.Back{ --test deck 1
-    name = "testdeck",
-    key = "testdeck",
-    pos = {x = 0, y = 4},
-    atlas = 'Decks',
-    atlas_key = 'testdeck',
-        pos = {
+    name = "testdeck",      -- name of the deck (in code)
+    key = "testdeck",       -- key used to call the deck 
+    pos = {x = 0, y = 4},   -- unsure what this does currently, come back to this !!
+    atlas = 'Decks',        -- atlas referenced for deck texture
+    atlas_key = 'testdeck', -- also unsure what this does, come back to this !!
+        pos = {             -- position of the texture on the atlas
         x = 0,
         y = 0,
         },
-    config = {
+    config = {              -- config for the deck, can include hands, discards, consumables, vouchers, money, etc.
         hands = 0,
         discards = -1,
         consumables = {'c_wraith'},
     },
 
     loc_txt = {
-        name = "Test Deck",
-        text = {
+        name = "Test Deck", -- name the deck appears as in game
+        text = {            -- description text for the deck
             "{C:red}#1#{} discards",
             "Start with a {C:spectral}Wraith{} card"
         }
     },
-    loc_vars = function(self)
+    loc_vars = function(self)   --variable setup
         return { vars = { 
-            self.config.discards,
-            self.config.hands,
-            self.config.consumables
+            self.config.discards,   -- #1#
+            self.config.hands,      -- #2#
+            self.config.consumables -- #3#
         }}
     end,
 
